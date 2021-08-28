e9a440c3c9ed99a056b9418235025f35bd58e339
Robert Nubla <rnubla97@gmail.com>
2021-08-28 11:11:25 -0400

    Added Components
    Navbar and MusicTable components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
+  <div class="relative">
+    <div class="mx-auto h-full" style="min-height: 85vh">
+      <Navbar />
+      <MusicTable />
+    </div>
+    <div>
+      <Player />
+    </div>
+  </div>
   <!-- <HelloWorld /> -->
-  <MainLayout />
+  <!-- <MainLayout /> -->
 </template>
 
 <script >
+import Navbar from "./components/Navigation/Navbar.vue";
 // import HelloWorld from "./components/HelloWorld.vue";
 import MainLayout from "./components/MainLayout.vue";
+import MusicTable from "./components/Table/MusicTable.vue";
+import Player from "./components/MusicPlayer/Player.vue";
 export default {
   components: {
+    Navbar,
     MainLayout,
+    MusicTable,
+    Player,
     // HelloWorld,
   },
 };
@@ -24,6 +39,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
