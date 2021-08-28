bf42a25c0ad88f9010e4a760568fbca80458f194
Robert Nubla <rnubla97@gmail.com>
2021-08-28 11:12:17 -0400

    Added tailwind index.css

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import store from "./store";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import "@vime/core/themes/default.css";
+import "./index.css";
 
 createApp(App).use(store).use(ElementPlus).mount("#app");
