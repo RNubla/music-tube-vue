a8b1020c610589c558a1d92402eea1dc678711f6
Robert Nubla <rnubla97@gmail.com>
2021-08-28 11:12:05 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' or 'class'
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["checked"],
+      borderColor: ["checked"],
+      inset: ["checked"],
+      zIndex: ["hover", "active"],
+    },
+  },
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+  },
+};
