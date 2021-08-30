9e7cc460441f7e43b644fc6c3e83a8d44c240b6c
Robert Nubla <rnubla97@gmail.com>
2021-08-30 16:26:36 -0400

    Added Fetching of audio url

--- src/components/Table/MusicTable.vue
+++ src/components/Table/MusicTable.vue
@@ -8,64 +8,28 @@
               <tr>
                 <th
                   scope="col"
-                  class="
-                    px-5
-                    py-3
-                    bg-white
-                    border-b border-gray-200
-                    text-gray-800 text-left text-sm
-                    uppercase
-                    font-normal
-                  "
-                >
-                  Title
-                </th>
+                  class="px-5 py-3 bg-white border-b border-gray-200 text-gray-800 text-left text-sm uppercase font-normal"
+                >Title</th>
                 <th
                   scope="col"
-                  class="
-                    px-5
-                    py-3
-                    bg-white
-                    border-b border-gray-200
-                    text-gray-800 text-left text-sm
-                    uppercase
-                    font-normal
-                  "
-                >
-                  Youtube Link
-                </th>
+                  class="px-5 py-3 bg-white border-b border-gray-200 text-gray-800 text-left text-sm uppercase font-normal"
+                >Youtube Link</th>
                 <th
                   scope="col"
-                  class="
-                    px-5
-                    py-3
-                    bg-white
-                    border-b border-gray-200
-                    text-gray-800 text-left text-sm
-                    uppercase
-                    font-normal
-                  "
-                >
-                  Added
-                </th>
+                  class="px-5 py-3 bg-white border-b border-gray-200 text-gray-800 text-left text-sm uppercase font-normal"
+                >Added</th>
                 <th
                   scope="col"
-                  class="
-                    px-5
-                    py-3
-                    bg-white
-                    border-b border-gray-200
-                    text-gray-800 text-left text-sm
-                    uppercase
-                    font-normal
-                  "
-                >
-                  Visit
-                </th>
+                  class="px-5 py-3 bg-white border-b border-gray-200 text-gray-800 text-left text-sm uppercase font-normal"
+                >Visit</th>
               </tr>
             </thead>
             <tbody>
-              <tr v-for="song in this.$store.state.songData" >
+              <tr
+                v-for="(song, index) in this.$store.state.songData"
+                @click.prevent="playSelectedSong(index)"
+                :class="{ 'bg-gray-500': (song.title === selectedSong) }"
+              >
                 <TableData :songValues="song" />
               </tr>
             </tbody>
@@ -77,39 +41,30 @@
 </template>
 
 <script>
+import { useStore } from "vuex";
 import TableData from "./TableData.vue";
 export default {
   components: {
     TableData,
   },
+  setup() {
+    const store = useStore()
+    let selectedSong = ''
+    const playSelectedSong = async (index) => {
+      selectedSong = store.state.songData[index].title
+      console.log(selectedSong)
+      await store.dispatch('fetchSongAudio', store.state.songData[index].youtubeUrl)
+    }
+
+    return {
+      selectedSong,
+      playSelectedSong
+    }
+
+  },
   data() {
     return {
-      songs: [
-        {
-          title: "Hello1",
-          link: "Link1",
-          added: "Added1",
-          Vist1: "Vist1",
-        },
-        {
-          title: "Hello2",
-          link: "Link2",
-          added: "Added2",
-          Vist1: "Vist2",
-        },
-        {
-          title: "Hello3",
-          link: "Link3",
-          added: "Added3",
-          Vist1: "Vist3",
-        },
-        {
-          title: "Hello4",
-          link: "Link4",
-          added: "Added4",
-          Vist1: "Vist4",
-        },
-      ],
+
     };
   },
 };
