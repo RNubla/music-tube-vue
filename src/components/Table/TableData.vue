7df6ac2c77563d6d4f25ed939162576b0864528d
Robert Nubla <rnubla97@gmail.com>
2021-08-30 16:27:05 -0400

    Play song when table row is selected

--- src/components/Table/TableData.vue
+++ src/components/Table/TableData.vue
@@ -10,9 +10,7 @@
           />
         </a>
       </div>
-      <div class="ml-3">
-        <p class="text-gray-900 whitespace-no-wrap">{{ songValues.title }}</p>
-      </div>
+      <div class="ml-3">{{ songValues.title }}</div>
     </div>
   </td>
   <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
@@ -23,43 +21,41 @@
   </td>
   <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
     <button @click.prevent="openLink">
-      <span
-        class="
-          relative
-          inline-block
-          px-3
-          py-1
-          font-semibold
-          text-green-900
-          leading-tight
-        "
-      >
-        <span
-          aria-hidden="true"
-          class="absolute inset-0 bg-green-200 opacity-50 rounded-full"
-        >
-        </span>
-        <span class="relative"> Watch Video </span>
+      <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+        <span aria-hidden="true" class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+        <span class="relative">Watch Video</span>
       </span>
     </button>
   </td>
 </template>
 
 <script>import { Command } from "@tauri-apps/api/shell";
+import { useStore } from "vuex";
 
 export default {
   props: {
-    songValues: {},
+    songValues: {
+      cover: '',
+      title: '',
+      youtubeUrl: '',
+      dateAdded: ''
+    },
   },
-  setup(props){
+  setup(props) {
+    const store = useStore()
+    const playSelectedSong = async () => {
+      await store.dispatch('fetchSongAudio', props.songValues.youtubeUrl)
+      // console.log('playSelectedSong', props.songValues.youtubeUrl)
+    }
     const openLink = () => {
       console.log('openLink')
       // open(props.songValues.youtubeUrl, 'edge')
       new Command('powershell', ['/C', `start ${props.songValues.youtubeUrl}`]).execute()
     }
-  return {
-    openLink
-  }
+    return {
+      openLink,
+      playSelectedSong
+    }
   }
 };
 </script>
