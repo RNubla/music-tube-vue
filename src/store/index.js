32b16284a47379008f4fe6ab1e9be3567aa93512
Robert Nubla <rnubla97@gmail.com>
2021-09-03 14:22:52 -0400

    Update index.js
    created actions to get url data

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
+import axios from "axios";
 import { createStore } from "vuex";
+import { Command } from "@tauri-apps/api/shell";
+import { fs } from "@tauri-apps/api";
 export default createStore({
   state: {
+    playlistIndex: 0,
     songEnded: false,
     currentSong: {},
     playlist: [],
@@ -14,6 +18,43 @@ export default createStore({
     },
     SET_SONG_ENDED(state, payload) {
       state.songEnded = payload;
+    },
+    APPEND_AUDIO_SRC(state, payload) {
+      let wantedSong = state.playlist.find((song) => song.id == payload.id);
+      let idx = state.playlist.indexOf(wantedSong);
+      wantedSong["src"] = payload.audioSrc;
+      state.playlist[idx] = wantedSong;
+    },
+    INCREMENT_PLAYLIST_INDEX(state) {
+      state.playlistIndex += 1;
+    },
+  },
+  actions: {
+    fetchAudioSrc({ commit }, payload) {
+      axios.post("http://127.0.0.1:8000/id", { v: payload }).then((res) => {
+        const data = res.data._ydl_info;
+        const m4a = data["formats"].filter((e) => e.ext == "m4a");
+        // const audioSrc = m4a[0].url
+        const newPayload = { id: payload, audioSrc: m4a[0].url };
+        commit("APPEND_AUDIO_SRC", newPayload);
+      });
+    },
+    saveToJson({ state }) {
+      const jsonData = JSON.stringify(state.playlist);
+      // var fs = require("fs");
+      fs.writeFile({ path: "test.json", contents: jsonData });
+    },
+    loadJson({ state }) {
+      // const jsonData =
+      fs.readTextFile("test.json").then(async (data) => {
+        // console.log("data", data);
+        if (data === undefined || data == "" || !data) {
+          console.log("No data is written on test.json");
+          state.playlist = [];
+        } else {
+          state.playlist = await JSON.parse(data);
+        }
+      });
     },
   },
 });
