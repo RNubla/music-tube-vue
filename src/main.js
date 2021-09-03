15dfe490c3f955ef383abf880eebd4b9ef76e74d
Robert Nubla <rnubla97@gmail.com>
2021-09-03 01:43:11 -0400

    Update main.js
    imported new version of store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import store from "./store";
+import store from "./store/index";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import "@vime/core/themes/default.css";
